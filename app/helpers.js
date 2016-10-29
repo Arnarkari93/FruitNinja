c833b9e687f7bb5a2feef8986a0040f82b197855
Luv Agarwal <luvagarwal.q@gmail.com>
2016-10-29 16:32:48 +0530

    intersection check helper added

---
+++ app/helpers.js
@@ -0,0 +1,28 @@
+export function isIntersecting(p1, p2, element) {
+  /*
+  Input: two points, a pixi element
+  Output: true if line segment joining p1 and p2 passes through element
+  */
+
+  function linearCombination(p1, p2, lambda) {
+    return {
+      'x': lambda * p1.x + (1 - lambda) * p2.x,
+      'y': lambda * p1.y + (1 - lambda) * p2.y
+    };
+  }
+
+  function distance(p1, p2) {
+    return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+  }
+
+  // Calculate the number of points to choose b/w the mouse points
+  // for intersection. Taking 50 points for every 1000px distance
+  let npoints = (50/1000) * distance(p1, p2);
+
+  for(let lambda=0; lambda <= 1; lambda += 1/npoints) {
+    let p = linearCombination(p1, p2, lambda);
+    if(element.containsPoint(new PIXI.Point(p.x, p.y)))
+      return true;
+  }
+  return false;
+}
