e054662df9a583fd65a26829ed5b2a4217dbebe1
Luv Agarwal <luvagarwal.q@gmail.com>
2016-10-31 13:19:32 +0530

    Feature additions

--- app/gameplay.js
+++ app/gameplay.js
@@ -9,6 +9,9 @@ export default class GamePlayContainer extends BaseContainer {
     super();
     this.mode = mode;
     this.loaded = false;
+    this.score = 0;
+    this.missed = 0;
+    this.timestart = +new Date;
     this.loadTextures();
   }
 
@@ -24,6 +27,10 @@ export default class GamePlayContainer extends BaseContainer {
 
   splashesLoaded() {
     console.log("Splashes loaded");
+  }
+
+  numsLoaded() {
+    console.log("Numbers loaded");
   }
 
   handleOptionSelection() {
@@ -47,6 +54,12 @@ export default class GamePlayContainer extends BaseContainer {
       .load(() => {
         let self = this;
         self.splashesLoaded();
+      });
+    PIXI.loader
+      .add('assets/nums.json')
+      .load(() => {
+        let self = this;
+        self.numsLoaded();
       });
   }
 
@@ -73,7 +86,7 @@ export default class GamePlayContainer extends BaseContainer {
     function intializeDetails() {
 
       let conf = {
-        x: Math.random()*Config.ww,
+        x: Math.random()*(Config.ww-200) + 50,
         y: Config.wh,
         vx: Math.max(1, Math.random()*Config.fruit.vx),
         vy: Config.fruit.vy,
@@ -144,20 +157,22 @@ export default class GamePlayContainer extends BaseContainer {
     function animateDrops(self) {
       // Remove drops with r <= 0 by reverse traversing
       let drops = self.getAll('drops');
-
       for(let drop of drops) {
-        if(drop.height <= 0)
-          self.remove('drops', drop.name);
-        else {
+        if(drop.height > 0) {
           // Move drops while decreasing size
-          drop.x += drop.vx;
-          drop.y += drop.vy;
-          drop.height -= 1;
-          drop.width -= 1;
+          let details = drop.details;
+          details.x += details.vx;
+          details.y += details.vy;
+          details.radius -= 1;
+          //details.visible = (Math.floor(Math.random()*2) == 0);
+          self.add('drops', self.getNewDrop(details));
+
+          /*drop.radius -= 1;*/
         }
+        self.remove('drops', drop.name);
       }
     }
-    /*animateDrops(this);*/
+    animateDrops(this);
 
     // Handle animations for cut fruits
     function animateCutFruits(self) {
@@ -189,18 +204,104 @@ export default class GamePlayContainer extends BaseContainer {
     }
     animateSplashes(this);
 
-    this.updateScore({
-      'missed': fruitsMissed,
-      'cut': noFruitCuts
-    });
+    this.score += noFruitCuts;
+    this.missed += fruitsMissed;
+
+    this.animateScoreBoard();
   }
 
-  initializeScore() {
-    /*let scoreBoard = */
+  animateScoreBoard() {
+    let score = this.score;
+    let chars = [];
+
+    while(score > 0) {
+      chars.unshift(score % 10);
+      score = Math.floor(score/10);
+    }
+    if(chars.length == 1)
+      chars.unshift(0);
+    if(chars.length == 0)
+      chars = [0, 0]
+
+    let x = 80;
+    let y = 50;
+
+    this.remove('scoreContainer');
+    for(let char of chars) {
+      let gr = new PIXI.Sprite(PIXI.Texture.fromFrame(`num${char}.png`));
+      gr.width = 40;
+      gr.height = 50;
+      gr.x = x;
+      gr.y = y;
+      x += 50;
+      this.add('scoreContainer', gr);
+    }
+
+    if(this.mode == "archade mode")
+      this.animateArchadeMode();
+    else if(this.mode == "zen mode")
+      this.animateZenMode();
   }
 
-  updateScore(data) {
+  animateArchadeMode() {
+    let seconds = 60 - Math.floor((+new Date - this.timestart)/1000);
+    if(seconds < 0) return;
+    let mins = Math.floor(seconds/60);
+    let secs = seconds % 60;
+    secs = [Math.floor(secs/10), secs%10];
 
+    let score = [mins, "10", ...secs]; // "10" is for ':'
+    let x = Config.ww - 250;
+    let y = 70;
+    this.remove('timeContainer');
+
+    for(let s of score) {
+      let gr = new PIXI.Sprite(PIXI.Texture.fromFrame(`num${s}.png`));
+      gr.x = x;
+      gr.y = y;
+      gr.width = 40;
+      gr.height = 50;
+
+      x += 50;
+
+      this.add('timeContainer', gr);
+    }
+  }
+
+  animateZenMode() {
+    let cross = [];
+    for(let i=0; i < this.missed && i < 3; i++) {
+      cross.push('r'); // red cross
+    }
+    while(cross.length < 3)
+      cross.push('b');
+
+    let x = Config.ww - 250;
+    let y = 100;
+    this.remove('crossContainer');
+    for(let c of cross) {
+      let cr = new PIXI.Sprite(PIXI.Texture.fromFrame(`${c}cross.png`));
+      cr.x = x;
+      cr.y = y;
+      cr.width = 30;
+      cr.height = 30;
+
+      x += 40;
+
+      this.add('crossContainer', cr);
+    }
+  }
+
+  getNewDrop(details) {
+    const drop = new PIXI.Graphics();
+    drop.lineStyle(2, details.color);
+    drop.beginFill(details.color, 1);
+    drop.drawCircle(details.x, details.y, details.radius);
+    drop.endFill();
+    drop.vx = details.vx; drop.vy = details.vy;
+    //drop.visible = details.visible;
+    drop.details = details;
+    return drop;
   }
 
   handleNewFruitCuts() {
@@ -218,16 +319,6 @@ export default class GamePlayContainer extends BaseContainer {
 
     function initializeDrops(self, fruit) {
 
-      function getNewDrop(details) {
-        const drop = new PIXI.Graphics();
-        drop.lineStyle(2, details.color);
-        drop.beginFill(details.color, 1);
-        drop.drawCircle(details.x, details.y, details.radius);
-        drop.endFill();
-        drop.vx = details.vx; drop.vy = details.vy;
-        return drop;
-      }
-
       for(let i = 0; i < 40; i++) {
         let vx = Math.floor(Math.random()*10);
         let vy = Math.floor(Math.random()*10);
@@ -244,9 +335,10 @@ export default class GamePlayContainer extends BaseContainer {
           vx,
           vy,
           radius,
-          color: mapping.color,
+          color: mapping.dropColor,
+          //visibleID: (Math.floor(Math.random()*2) == 0)
         };
-        self.add('drops', getNewDrop(details));
+        self.add('drops', self.getNewDrop(details));
       }
     }
 
@@ -298,7 +390,7 @@ export default class GamePlayContainer extends BaseContainer {
     let fruits = this.getAll('fruits');
     for(let fruit of fruits) {
       // No intersection
-      if(!checkIfIntersection(mouseData, fruit))
+      if(!checkIfIntersection(mouseData, fruit.getBounds()))
         continue
 
       noFruitCuts += 1;
@@ -308,6 +400,7 @@ export default class GamePlayContainer extends BaseContainer {
       initializeCutFruit(this, fruit);
       this.remove('fruits', fruit.name);
     }
+    return noFruitCuts;
   }
 
   resize() {
