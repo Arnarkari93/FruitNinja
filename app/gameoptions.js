bc56f9e4fa862c0579f29938cda96eedb992e431
Luv Agarwal <luvagarwal.q@gmail.com>
2016-11-02 16:03:13 +0530

    lint fixes

--- app/gameoptions.js
+++ app/gameoptions.js
@@ -1,11 +1,14 @@
 import PIXI from 'pixi.js';
-import BaseContainer from './basecontainer.js';
-import { isIntersecting } from './helpers.js';
-import { LoaderContainer } from './others.js';
+
+import BaseContainer from './basecontainer';
+import { isIntersecting } from './helpers';
+import { LoaderContainer } from './others';
 
 export class OptionsContainer extends BaseContainer {
-  // Responsible for drawing options on board
-  // This assumes that assets are already loaded
+  /**
+   * Responsible for drawing options on board
+   * This assumes that assets are already loaded
+   */
 
   constructor(options) {
     super();
@@ -18,61 +21,59 @@ export class OptionsContainer extends BaseContainer {
   }
 
   drawCircles() {
-    for(let i=0; i<3; i++) {
-      let option = new PIXI.Sprite(PIXI.Texture.fromFrame('circle.png'));
+    for(let i = 0; i < 3; i += 1) {
+      const option = new PIXI.Sprite(PIXI.Texture.fromFrame('circle.png'));
       option.width = 200; option.height = 200;
-      option.x = 250*i;
-      if(i == 1) option.y = -100;
+      option.x = 250 * i;
+      if (i == 1) option.y = -100;
       this.add('optionCircles', option);
     }
   }
 
   detectSelection(mouseData) {
+    if (mouseData.length < 2) return;
 
-    if(mouseData.length < 2) return;
+    const [p1, p2] = mouseData;
 
-    let [p1, p2] = mouseData;
+    const optionCircles = this.getAll('optionCircles');
 
-    let optionCircles = this.getAll('optionCircles');
-
-    for(let i=0; i < optionCircles.length; i++) {
-      let option = optionCircles[i];
-      if(isIntersecting(p1, p2, option.getBounds()))
+    for(let i = 0; i < optionCircles.length; i += 1) {
+      const option = optionCircles[i];
+      if (isIntersecting(p1, p2, option.getBounds()))
         return this.optionNames[i];
     }
-    return;
   }
 
   drawText() {
-    let imageExtraPadding = 20;
-    let optionCircles = this.getAll('optionCircles');
+    const imageExtraPadding = 20;
+    const optionCircles = this.getAll('optionCircles');
 
-    for(let i=0; i < optionCircles.length; i++){
-      let option = optionCircles[i];
-      let r = option.width/2;
+    for (let i = 0; i < optionCircles.length; i += 1) {
+      const option = optionCircles[i];
+      const r = option.width/2;
       let text = this.optionNames[i].split(" ");
       text.forEach((word, i) => {
         word = word[0].toUpperCase() + word.slice(1);
         text[i] = word;
       });
       text = text.join(" ");
-      let textContainer = new TextOnPerimiterContainer({
-                              'x': option.x + r,
-                              'y': option.y + r
-                            }, r - imageExtraPadding,
-                            text
-                          );
+
+      const textContainer = new TextOnPerimiterContainer({
+        'x': option.x + r,
+        'y': option.y + r
+      }, r - imageExtraPadding, text);
       this.add('textsContainer', textContainer);
     }
   }
 
   drawFruit() {
-    let optionCircles = this.getAll('optionCircles');
+    const optionCircles = this.getAll('optionCircles');
 
-    for(let i=0; i < optionCircles.length; i++){
-      let option = optionCircles[i];
-      let r = option.width/2;
-      let fruit = new PIXI.Sprite(PIXI.Texture.fromFrame(`option${i}.png`));
+    for (let i = 0; i < optionCircles.length; i += 1) {
+      const option = optionCircles[i];
+      const r = option.width/2;
+
+      const fruit = new PIXI.Sprite(PIXI.Texture.fromFrame(`option${i}.png`));
       fruit.anchor.x = 0.5; fruit.anchor.y = 0.5;
       fruit.width = 4*r/5; fruit.height = 4*r/5;
       fruit.x = option.x + r; fruit.y = option.y + r;
@@ -89,13 +90,13 @@ export class OptionsContainer extends BaseContainer {
     // animate fruits
     this.getAll('fruitsContainer').forEach((fruit, i) => {
       let rotation = 0.05;
-      if(i == 1)
+      if (i == 1)
         fruit.rotation -= rotation;
       else
         fruit.rotation += rotation;
     });
   }
-// End class
+
 }
 
 export class TextOnPerimiterContainer extends BaseContainer {
@@ -106,20 +107,21 @@ export class TextOnPerimiterContainer extends BaseContainer {
     this.r = radius;
     this.text = text;
     this.style = {
-              fontFamily:'Arial',
-              fontSize: 24,
-              fill: '#686868',
-              align: 'center',
-              fontWeight: 'bolder',
-            };
+      fontFamily:'Arial',
+      fontSize: 24,
+      fill: '#686868',
+      align: 'center',
+      fontWeight: 'bolder',
+    };
     this.drawText();
   }
 
   drawText() {
     // PIXI pivot abnormal behaviour
+
     let theta = Math.PI/4;
-    for(let i=0; i<this.text.length; i++) {
-      let text = new PIXI.Text(this.text[i], this.style);
+    for(let i = 0; i < this.text.length; i += 1) {
+      const text = new PIXI.Text(this.text[i], this.style);
       text.anchor.x = 0.5; text.anchor.y = 0.5;
       text.pivot.x = 0;
       text.x = this.center.x;
@@ -135,12 +137,14 @@ export class TextOnPerimiterContainer extends BaseContainer {
       text.rotation += 0.01;
     });
   }
-// End class
+
 }
 
 export class GameLabelContainer extends BaseContainer {
-  // Responsible for displaying label "Fruit Ninja" on board
-  // This assumes that assets are already loaded
+  /**
+   * Responsible for displaying label "Fruit Ninja" on board
+   * This assumes that assets are already loaded
+   */
 
   constructor(...args) {
     super(...args);
@@ -150,11 +154,12 @@ export class GameLabelContainer extends BaseContainer {
   }
 
   drawLabel() {
-    let fruit = new PIXI.Sprite(PIXI.Texture.fromFrame('logofruit.png'));
+    const fruit = new PIXI.Sprite(PIXI.Texture.fromFrame('logofruit.png'));
     fruit.width = 400; fruit.height = 150;
-    let ninja = new PIXI.Sprite(PIXI.Texture.fromFrame('logoninja.png'));
-    ninja.x = 450; ninja.width = 300; ninja.height = 150;
     this.addChild(fruit);
+
+    const ninja = new PIXI.Sprite(PIXI.Texture.fromFrame('logoninja.png'));
+    ninja.x = 450; ninja.width = 300; ninja.height = 150;
     this.addChild(ninja);
   }
 }
@@ -170,7 +175,7 @@ export default class GameOptionsContainer extends BaseContainer {
 
   handleOptionSelection() {
     let mouseData = this.parent.mouseData;
-    if(this.get('optionsContainer') == undefined) return;
+    if (this.get('optionsContainer') == undefined) return;
     let selected = this.get('optionsContainer').detectSelection(mouseData);
     return selected;
   }
@@ -185,13 +190,13 @@ export default class GameOptionsContainer extends BaseContainer {
   }
 
   animate() {
-    if((this.parent.filesLoaded < this.parent.filesToLoad) ||
+    if ((this.parent.filesLoaded < this.parent.filesToLoad) ||
       (+new Date - this.timestart)/1000 < 0.5){
       this.animateLoader();
       return;
     }
 
-    if(this.loading) {
+    if (this.loading) {
       this.loading = false;
       window.dispatchEvent(new Event('resize'));
     }
@@ -199,16 +204,15 @@ export default class GameOptionsContainer extends BaseContainer {
     this.remove('loaderContainer');
 
     // Draw stuff on load once
-    if(this.get('gameLabelContainer') == undefined) {
-      let gameLabelContainer = new GameLabelContainer();
+    if (this.get('gameLabelContainer') == undefined) {
+      const gameLabelContainer = new GameLabelContainer();
       this.add('gameLabelContainer', gameLabelContainer);
     }
 
-    if(this.get('optionsContainer') == undefined) {
-      let optionsContainer = new OptionsContainer(this.optionNames);
+    if (this.get('optionsContainer') == undefined) {
+      const optionsContainer = new OptionsContainer(this.optionNames);
       this.add('optionsContainer', optionsContainer);
-    }
-    else
+    } else
       this.get('optionsContainer').animate();
   }
 }
