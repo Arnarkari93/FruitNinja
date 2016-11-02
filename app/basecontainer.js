8e62229c237b0ad1de8740982ef792b1d7ca4533
Luv Agarwal <luvagarwal.q@gmail.com>
2016-11-02 16:01:41 +0530

    documentation, eslint fixes

--- app/basecontainer.js
+++ app/basecontainer.js
@@ -1,4 +1,36 @@
+/**
+ * It implements a base container which abstracts the add, get and
+ * removal of PIXI elements. In general, whenever a PIXI element is added
+ * in container, a reference has to be stored to access it later. There is
+ * also a need to store a set of elements for ex. all fruits as a list of
+ * set. Same goes for getting and deleting these elements. BaseContainer
+ * nicely abstract those things.
+ *
+ * > let fruit = new PIXI.Graphics();
+ *
+ * Now, in general, to add an element:
+ * > fruit.name = <someid>
+ * > this.addChild(fruit);
+ * > this.fruits = new Set(); # if not created already
+ * > this.fruits.add(fruit.name) # save name to access element later
+ *
+ * To delete:
+ * > for(let e of this.fruits)
+ * >   this.removeChild(e);
+ * > delete this.fruits;
+
+ * With BaseContainer, this can be done as
+ * > this.add('fruits', fruit)
+ *
+ * To get all fruits elements, do
+ * > this.getAll('fruits')
+ *
+ * To remove all fruits element, do
+ * > this.remove('fruits')
+ */
+
 import PIXI from 'pixi.js';
+
 
 export default class BaseContainer extends PIXI.Container {
 
@@ -6,56 +38,69 @@ export default class BaseContainer extends PIXI.Container {
     super(...args);
   }
 
-  randomName() {
+  static randomUID() {
     return `${Math.random()}`;
   }
 
-  add(name, child, at=null) {
-    if(this[name] == undefined)
+  add(name, child, at = null) {
+    // Make new set container if not existing
+    if (this[name] === undefined)
       this[name] = new Set();
 
-    let uid = this.randomName();
+    const uid = BaseContainer.randomUID();
+
+    if (child === undefined)
+      throw `ChildUndefined: ${name}`;
     child.name = uid;
     this[name].add(uid);
-    if(at == null)
+
+    if (at == null)
       this.addChild(child);
     else
       this.addChildAt(child, at);
   }
 
-  get(name, uid=null) {
-    if(this[name] == undefined || this[name].size == 0)
+  get(name, uid = null) {
+    if (this[name] === undefined || this[name].size === 0)
       return undefined;
 
-    if(uid == null)
+    /**
+     * return first element (since `get` is only called for name
+     * references having single elements otherwise uid is passed)
+     */
+    if (uid === null)
       return this.getChildByName(this[name].keys().next().value);
     return this.getChildByName(uid);
-
   }
 
   getAll(name) {
-    if(this[name] == undefined)
+    // Get all elements corresponding to `name` reference
+
+    if (this[name] === undefined)
       return [];
 
-    let children = [];
+    const children = [];
 
     for(let e of this[name])
       children.push(this.getChildByName(e));
 
     return children;
-
   }
 
-  remove(name, uid=null) {
-    if(uid == null) {
-      for(let e of this.getAll(name)){
+  remove(name, uid = null) {
+    /**
+     * Remove all elements corresponding to `name` reference
+     * if uid not given, else remove element with name uid.
+     */
+    if (uid === null) {
+      for(let e of this.getAll(name))
         this.removeChild(e);
-      }
+
       delete this[name];
-    }
-    else {
+    } else {
       this.removeChild(this.get(name, uid));
       this[name].delete(uid);
     }
   }
+
 }
