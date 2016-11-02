332a49979027625b943e6c80bd38af90d2ae0530
Luv Agarwal <luvagarwal.q@gmail.com>
2016-11-02 21:40:24 +0530

    code refactoring

--- app/app.js
+++ app/app.js
@@ -1,11 +1,13 @@
-import './index.html';
 import PIXI from 'pixi.js';
-import BaseContainer from './basecontainer.js';
-import Knife from './knife.js';
-import GameOptionsContainer from './gameoptions.js';
-import GamePlayContainer from './gameplay.js';
-import { HighScoreContainer, AboutGameContainer, LoaderContainer } from './others.js';
-import { Config } from './config.js';
+
+import './index.html';
+import BaseContainer from './basecontainer';
+import Knife from './knife';
+import GameOptionsContainer from './gameoptions';
+import GamePlayContainer from './gameplay';
+import { HighScoreContainer, AboutGameContainer, LoaderContainer } from './others';
+import { Config } from './config';
+
 
 const renderer = PIXI.autoDetectRenderer(window.innerWidth,
   window.innerHeight, {
@@ -21,16 +23,24 @@ document.body.appendChild(renderer.view);
 class Root extends BaseContainer {
   constructor(...args) {
     super(...args);
-    this.h = Config.wh;
-    this.w = Config.ww;
     this.interactive = true;
 
-    this.pause = false;
-    this.cutting = false; // true if Knife in cutting mode
-    this.mouseData = []; // data of mouse movement
+    this.h = Config.wh;
+    this.w = Config.ww;
 
+    this.pause = false;
+    // true if Knife in cutting mode
+    this.cutting = false;
+    // mouse movement data
+    this.mouseData = [];
+
+    // used as a trigger for resizing
     this.containerChange = true;
+    this.state = "initial";
     this.counter = 0;
+
+    this.filesToLoad = 1;
+    this.filesLoaded = 0;
     this
       .on('mousedown', this.onMouseDown())
       .on('touchstart', this.onMouseDown())
@@ -42,19 +52,15 @@ class Root extends BaseContainer {
       .on('touchendoutside', this.onMouseUp())
       //.on('click', this.onClick)
 
-    this.state = "initial";
-    this.filesToLoad = 1;
-    this.filesLoaded = 0;
-
-    let options = ["about game", "new game", "high score"];
-    let gameContainer = new GameOptionsContainer(options);
+    const options = ["about game", "new game", "high score"];
+    const gameContainer = new GameOptionsContainer(options);
     this.add('gameContainer', gameContainer);
 
     this.loadTextures();
   }
 
   gameInit() {
-    let bg = new PIXI.Sprite(PIXI.Texture.fromFrame('bg.png'));
+    const bg = new PIXI.Sprite(PIXI.Texture.fromFrame('bg.png'));
     bg.height = this.h; bg.width = this.w;
     bg.interactive = true;
     this.add('bg', bg, 0);
@@ -70,30 +76,28 @@ class Root extends BaseContainer {
     PIXI.loader
       .add('assets/basics.json')
       .load(() => {
-        let self = this;
-        self.assetsLoaded();
+        this.assetsLoaded();
       });
   }
 
-  onClick(x) {
+  onClick(e) {
     this.pause = !this.pause;
   }
 
   onMouseDown() {
-    return (x) => {
-      let self = this;
-      self.cutting = true;
-      let position = x.data.global;
-      self.mouseData.push({
+    return (e) => {
+      this.cutting = true;
+      let position = e.data.global;
+      this.mouseData.push({
         x: position.x,
         y: position.y
       });
-      self.add('knife', new Knife());
+      this.add('knife', new Knife());
     };
   }
 
   onMouseUp() {
-    return (x) => {
+    return (e) => {
       this.cutting = false;
       this.remove('knife');
       this.mouseData = [];
@@ -101,25 +105,27 @@ class Root extends BaseContainer {
   }
 
   onMouseMove() {
-    let self = this;
-    return (x) => {
-      let position = x.data.global;
-      if(self.cutting) {
-        self.mouseData.push({
+    return (e) => {
+      let position = e.data.global;
+      if(this.cutting) {
+        this.mouseData.push({
           x: position.x,
           y: position.y
         });
 
         // Keep only last two datapoints
-        if(self.mouseData.length == 3)
-          self.mouseData.shift();
+        if(this.mouseData.length == 3)
+          this.mouseData.shift();
       }
     };
   }
 
   reduceInitial(action) {
-    // Reduce from Initial state to next state
-    // Possible actions: about game, new game, high score
+    /**
+     * Reduce from Initial state to next state
+     * Possible actions: about game, new game, high score
+     */
+
     let gameContainer;
 
     switch(action) {
@@ -127,7 +133,7 @@ class Root extends BaseContainer {
         gameContainer = new AboutGameContainer();
         break;
       case "new game":
-        let options = ["archade mode", "zen mode", "back"];
+        const options = ["archade mode", "zen mode", "back"];
         gameContainer = new GameOptionsContainer(options);
         break;
       case "high score":
@@ -139,6 +145,7 @@ class Root extends BaseContainer {
 
   reduceNewGame(action) {
     // Possible actions: zen mode, archade mode, back
+
     let gameContainer;
 
     switch(action) {
@@ -147,7 +154,7 @@ class Root extends BaseContainer {
         gameContainer = new GamePlayContainer(action);
         break;
       case "back":
-        let options = ["about game", "new game", "high score"];
+        const options = ["about game", "new game", "high score"];
         gameContainer = new GameOptionsContainer(options);
         break;
     }
@@ -155,9 +162,9 @@ class Root extends BaseContainer {
   }
 
   reduce(action) {
-    // Switch to new state based on action
+    // Return new state given action
 
-    let prevState = this.state;
+    const prevState = this.state;
 
     if(this.state == "about game" || this.state == "high score")
       this.state = "initial";
@@ -173,7 +180,7 @@ class Root extends BaseContainer {
       case "about game":
       case "high score":
         // go back
-        let options = ["about game", "new game", "high score"];
+        const options = ["about game", "new game", "high score"];
         gameContainer = new GameOptionsContainer(options);
         break;
       case "new game":
@@ -186,7 +193,6 @@ class Root extends BaseContainer {
   animate() {
     if(this.pause)
       return;
-
 
     this.get('gameContainer').animate();
 
@@ -202,7 +208,6 @@ class Root extends BaseContainer {
       this.get('knife').animate();
   }
 
-// End class
 }
 
 const stage = new Root();
@@ -211,41 +216,38 @@ let prev = null;
 
 function resizeGameContainer() {
 
-  let gameContainer = stage.get('gameContainer');
-  let state = stage.state;
-  if(state == "archade mode" || state == "zen mode") {
-    if(stage.containerChange) {
-      gameContainer.scale.x *= window.innerWidth/Config.ww;
-      gameContainer.scale.y *= window.innerHeight/Config.wh;
+  const gameContainer = stage.get('gameContainer');
+  const state = stage.state;
+
+  if (state == "archade mode" || state == "zen mode") {
+    if (stage.containerChange) {
+      gameContainer.scale.x *= window.innerWidth / Config.ww;
+      gameContainer.scale.y *= window.innerHeight / Config.wh;
       stage.containerChange = false;
+    } else {
+      gameContainer.scale.x *= renderer.width / stage.w - 0.1;
+      gameContainer.scale.y *= renderer.height / stage.h;
     }
-    else {
-      gameContainer.scale.x *= renderer.width/stage.w - 0.1;
-      gameContainer.scale.y *= renderer.height/stage.h;
-    }
-  }
-  else {
-    if(gameContainer.loading) {
+  } else {
+    if (gameContainer.loading) {
       prev = {'x': gameContainer.scale.x, 'y': gameContainer.scale.y};
-      gameContainer.scale.x *= window.innerWidth/Config.ww;
-      gameContainer.scale.y *= window.innerHeight/Config.wh;
-    }
-    else{
+      gameContainer.scale.x *= window.innerWidth / Config.ww;
+      gameContainer.scale.y *= window.innerHeight / Config.wh;
+    } else {
       if(prev != null) {
         gameContainer.scale.x = prev.x;
         gameContainer.scale.y = prev.y;
       }
 
-      if(stage.containerChange) {
-        let scale = window.innerHeight/Config.wh;
+      if (stage.containerChange) {
+        let scale = window.innerHeight / Config.wh;
         if(window.innerHeight < 400) scale += 0.05;
-        // let scale = window.innerWidth/Config.ww;
+        //  let scale = window.innerWidth / Config.ww;
         gameContainer.scale.x *= scale;
         gameContainer.scale.y *= scale;
         stage.containerChange = false;
-      }
-      else {
-        let scale = renderer.height/stage.h;
+      } else {
+        let scale = renderer.height / stage.h;
         if(renderer.width < 400) scale += 0.05;
         // let scale = window.innerWidth/Config.ww;
         gameContainer.scale.x *= scale;
@@ -256,52 +258,17 @@ function resizeGameContainer() {
 }
 
 function resize(){
-  // Called after load is complete
+
   renderer.resize(window.innerWidth, window.innerHeight);
 
-  if(stage.filesLoaded == stage.filesToLoad) {
-    let bg = stage.get('bg');
-    bg.scale.x *= renderer.width/stage.w;
-    bg.scale.y *= renderer.height/stage.h;
-    // resize transition animation too
+  if (stage.filesLoaded == stage.filesToLoad) {
+    const bg = stage.get('bg');
+    bg.scale.x *= renderer.width / stage.w;
+    bg.scale.y *= renderer.height / stage.h;
     resizeGameContainer();
   }
-
   stage.w = renderer.width;
   stage.h = renderer.height;
-}
-/*
-
-function resizeGameContainer() {
-  let gameContainer = stage.get('gameContainer');
-  let state = gameContainer.state;
-  if(state == "archade mode" || state == "zen mode") {
-    gameContainer.scale.x *= window.innerWidth/Config.ww;
-    gameContainer.scale.y *= window.innerHeight/Config.wh;
-  }
-  else {
-    let scale = window.innerHeight/Config.wh;
-    if(window.innerHeight < 400) scale += 0.05;
-    // let scale = window.innerWidth/Config.ww;
-    gameContainer.scale.x *= scale;
-    gameContainer.scale.y *= scale;
-  }
-}
-
-function resize(){
-  // Called after load is complete
-
-  let bg = stage.get('bg');
-  bg.scale.x *= window.innerWidth/Config.ww;
-  bg.scale.y *= window.innerHeight/Config.wh;
-  // resize transition animation too
-
-  resizeGameContainer();
-  renderer.resize(window.innerWidth, window.innerHeight);
-}*/
-
-function resizeTransitionContainer() {
-
 }
 
 function animate() {
